94ec76ac0a384c9b54504b43e6b50e4f28fde3ee
Mohammad Ashif <mohammadashif510@gmail.com>
2023-08-19 01:09:41 +0600

    parallelogram added

--- area-calculate.js
+++ area-calculate.js
@@ -1,26 +1,39 @@
 function calculateTriangleArea(){
-    const inputFieldBase = document.getElementById('base');
-    const inputFieldBaseValue = parseFloat(inputFieldBase.value);
+    const triangleWidth = getElementValueById('base');
+    const triangleHeight = getElementValueById('height');
 
-
-    const inputFieldHeight = document.getElementById('height');
-    const inputFieldHeightValue = parseFloat(inputFieldHeight.value);
-
-    const area = 0.5 * inputFieldBaseValue * inputFieldHeightValue;
+    const area = 0.5 * triangleWidth * triangleHeight;
 
     const totalArea = document.getElementById('total-area');
     totalArea.innerText = area;
 }
 
+
 function calculateRectangleArea(){
-    const inputFiendWidth = document.getElementById('rectangle-width');
-    const inputFieldsWidthValue = parseFloat(inputFiendWidth.value);
+    const rectangleWidth = getElementValueById('rectangle-width');
+    const rectangleHeight = getElementValueById('rectangle-height');
 
-    const inputFieldRectangleHeight = document.getElementById('rectangle-height');
-    const inputFieldRectangleHeightValue = parseFloat(inputFieldRectangleHeight.value);
-
-    const area = inputFieldsWidthValue * inputFieldRectangleHeightValue;
+    const area = rectangleWidth * rectangleHeight;
 
     const totalArea = document.getElementById('rectangle-area');
     totalArea.innerText = area;
+}
+
+
+function calculateParallelogramArea(){
+    const baseValue = getElementValueById('parallelogram-base');
+    const heightValue = getElementValueById('parallelogram-height');
+
+    const totalArea = baseValue * heightValue;
+    
+    const area = document.getElementById('parallelogram-area');
+    area.innerText = totalArea;
+}
+
+// common function
+
+function getElementValueById(elementById){
+    const inputFiled = document.getElementById(elementById);
+    const inputFieldValue = parseFloat(inputFiled.value);
+    return inputFieldValue;
 }
