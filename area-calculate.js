1804f5a2b2b92a2e01a4e6bd3ab007e3b09fcc83
Mohammad Ashif <mohammadashif510@gmail.com>
2023-08-19 16:32:58 +0600

    project done

--- area-calculate.js
+++ area-calculate.js
@@ -2,10 +2,15 @@ function calculateTriangleArea(){
     const triangleWidth = getElementValueById('base');
     const triangleHeight = getElementValueById('height');
 
+    if(isNaN(triangleWidth) || isNaN(triangleHeight)){
+        alert('please inter a valid Number');
+        return;
+    }
+    // errorHandling(triangleHeight, triangleWidth);
     const area = 0.5 * triangleWidth * triangleHeight;
+    setElementInnerText('total-area', area);
 
-    const totalArea = document.getElementById('total-area');
-    totalArea.innerText = area;
+
 }
 
 
@@ -13,10 +18,14 @@ function calculateRectangleArea(){
     const rectangleWidth = getElementValueById('rectangle-width');
     const rectangleHeight = getElementValueById('rectangle-height');
 
+    if(isNaN(rectangleWidth) || isNaN(rectangleHeight)){
+        alert('please inter a valid Number');
+        return;
+    }
+
     const area = rectangleWidth * rectangleHeight;
 
-    const totalArea = document.getElementById('rectangle-area');
-    totalArea.innerText = area;
+    setElementInnerText('rectangle-area', area);
 }
 
 
@@ -24,10 +33,57 @@ function calculateParallelogramArea(){
     const baseValue = getElementValueById('parallelogram-base');
     const heightValue = getElementValueById('parallelogram-height');
 
+    if(isNaN(baseValue) || isNaN(heightValue)){
+        alert('please inter a valid Number');
+        return;
+    }
+
     const totalArea = baseValue * heightValue;
     
-    const area = document.getElementById('parallelogram-area');
-    area.innerText = totalArea;
+    setElementInnerText('parallelogram-area', totalArea);
+}
+
+function calculateEllipseArea(){
+    const a = getElementValueById('ellipse-major-axis');
+    const b = getElementValueById('ellipse-minor-axis');
+
+    if(isNaN(a) || isNaN(b)){
+        alert('please inter a valid Number');
+        return;
+    }
+
+    const area = Math.PI*a*b;
+
+    setElementInnerText('ellipse-area', area.toFixed(2));
+}
+
+function calculatePentagonArea(){
+    const perimeter = getElementValueById('pentagon-perimeter');
+    const apothem = getElementValueById('pentagon-apothem');
+
+    if(isNaN(perimeter) || isNaN(apothem)){
+        alert('please inter a valid Number');
+        return;
+    }
+
+    const area = 1/2 * perimeter * apothem;
+
+    setElementInnerText('pentagon-area', area);
+}
+
+
+function calculateRhombusArea(){
+    const diagonalP = getElementValueById('diagonalP');
+    const diagonalQ = getElementValueById('diagonalQ');
+
+    if(isNaN(diagonalP) || isNaN(diagonalQ)){
+        alert('please inter a valid Number');
+        return;
+    }
+
+    const area = (diagonalP * diagonalQ) / 2;
+
+    setElementInnerText('rhombus-area', area);
 }
 
 // common function
@@ -37,3 +93,16 @@ function getElementValueById(elementById){
     const inputFieldValue = parseFloat(inputFiled.value);
     return inputFieldValue;
 }
+
+function setElementInnerText(elementById, area){
+    const element = document.getElementById(elementById);
+    element.innerText = area;
+}
+
+
+// function errorHandling(value1, value2){
+//     if(isNaN(value1) || isNaN(value2)){
+//         alert('Please give a valid value');
+//         return
+//     }
+// }
